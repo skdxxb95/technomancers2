ef9cedb2702be2fbfc3ab838d1029bc83a332466
skdxxb95 <s3992963@student.rmit.edu.au>
2023-02-21 17:28:12 +1000

    Added textboxes for content

--- src/components/ProjectIdea/ProjectIdea.jsx
+++ src/components/ProjectIdea/ProjectIdea.jsx
@@ -10,7 +10,14 @@ const ProjectIdea = () => {
           <h1>DuckyPass</h1>
           <h5>Our Project Idea.</h5>
           <img src={PICO}></img>
-          
+          <div>
+            <h2>Test</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Hello 2</h2>
+            <p>Alright Sarah's stuff can go here..</p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/TeamProfile/TeamProfile.jsx
+++ src/components/TeamProfile/TeamProfile.jsx
@@ -9,6 +9,14 @@ const TeamProfile = () => {
         <div className="teamprofile-content">
           <h1>Team Profile</h1>
           <h5>Reflection on our last assignment and career plans.</h5>
+          <div>
+            <h3>Test</h3>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Hello hellooo</h2>
+            <p>Sarah's stuff can go here too..</p>
+          </div>
         </div>
       </div>
     </section>
