29975c440a487c4b93a0e17ed024b538164bfc15
Sarah Sinclair <120006918+stinclair@users.noreply.github.com>
2023-02-21 19:01:27 +1100

    added team profile and roles

--- src/components/TeamProfile/TeamProfile.jsx
+++ src/components/TeamProfile/TeamProfile.jsx
@@ -14,8 +14,12 @@ const TeamProfile = () => {
             <p>Hellooo</p>
           </div>
           <div>
-            <h2>Hello hellooo</h2>
-            <p>Sarah's stuff can go here too..</p>
+          <h1>Career Plans</h1>
+          <p>Discussion with the group indicates that there is no real significant shift in our ideal job outcomes after completing assignment 1 and 2. We all feel that we will remain on course to continue learning more about our ideal roles and what is required to achieve success.</p>
+          <p>All job prospects of our team members involve skills in analysis or problem-solving, strong communication skills, and the ability to work as an integral and high functioning member of a team.</p>
+          <p>As a group there are also some differences among our career aspirations, in particular with the type of industry pathways we wish to pursue and the technologies we may focus on. The differences that also stand out are the specific skills pathway such as cyber security and software development.</p>
+          <p>Also, some of the roles we all wish to pursue would require varied levels of experience to achieve the desired role, which means that we would need to pursue other entry level roles first to gain the required experience before being ready to take on our ideal job.</p>
+          <p>As mentioned in our previous assignment, despite the differences, our ideal jobs are all important components of an IT development software pathway. And can all contribute in different ways to the successful delivery of a software solution.</p>
           </div>
         </div>
       </div>
