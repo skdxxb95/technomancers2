0e1df664c68ee08ae283c905534514a56e7553fe
skdxxb95 <s3992963@student.rmit.edu.au>
2023-02-22 09:37:56 +1000

    Added more content and fixed styles

--- src/components/GroupReflection/GroupReflection.jsx
+++ src/components/GroupReflection/GroupReflection.jsx
@@ -6,11 +6,31 @@ const GroupReflection = () => {
     <section id='groupreflection'>
       <div className='groupreflection'>
         <div className="groupreflection-content">
-            <h1>Group Reflection</h1>
-            <h5>Our view on how things went.</h5>
-            <div>
-              <h3>Test</h3>
-              <p>Hellooo</p>
+          <h1>Group Reflection</h1>
+          <h5>Our view on how things went.</h5>
+          <div>
+            <h2>Sarah S.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Sam K.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Sam B.</h2>
+            <p>Reflecting on the recent group assignment, I feel proud of how well our team was able to communicate and support one another throughout the process. Even though we faced challenges with discrepancies in university enrolment, we were able to work through them together and keep each other motivated. I also recognize that our morale could have been improved during and after the emotional dip that we experienced. It was challenging to stay positive and focused, but I am proud of our ability to come together and muster up the motivation needed to finish the assignment. Through this experience, I have learned the importance of contributing to a team and being a supportive member. I now understand that working in a group can not only be effective in getting work done, but also provide a great support network. I am grateful for the opportunity to work with such a great team and for the lessons that I have learned. One aspect of the assignment that I found surprising was how evenly distributed the work was despite the difficulties we faced. However, I do not think that the GitHub log accurately represents the distribution of work. In my opinion, it was definitely an even split and I think we all contributed equally to the project. </p>
+          </div>
+          <div>
+            <h2>Ally H.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Damian W.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Reflection as a Group</h2>
+            <p>Hellooo</p>
           </div>
         </div>
       </div>
--- src/components/TeamProfile/TeamProfile.jsx
+++ src/components/TeamProfile/TeamProfile.jsx
@@ -10,12 +10,32 @@ const TeamProfile = () => {
           <h1>Team Profile</h1>
           <h5>Reflection on our last assignment and career plans.</h5>
           <div>
-            <h3>Test</h3>
+            <h2>Group Processes</h2>
             <p>Hellooo</p>
           </div>
           <div>
-          <h2>Career Plans</h2>
-          <p>Discussion with the group indicates that there is no real significant shift in our ideal job outcomes after completing assignment 1 and 2. We all feel that we will remain on course to continue learning more about our ideal roles and what is required to achieve success. All job prospects of our team members involve skills in analysis or problem-solving, strong communication skills, and the ability to work as an integral and high functioning member of a team. As a group there are also some differences among our career aspirations, in particular with the type of industry pathways we wish to pursue and the technologies we may focus on. The differences that also stand out are the specific skills pathway such as cyber security and software development. Also, some of the roles we all wish to pursue would require varied levels of experience to achieve the desired role, which means that we would need to pursue other entry level roles first to gain the required experience before being ready to take on our ideal job. As mentioned in our previous assignment, despite the differences, our ideal jobs are all important components of an IT development software pathway. And can all contribute in different ways to the successful delivery of a software solution.</p>
+            <h2>Career Plans</h2>
+            <p>Discussion with the group indicates that there is no real significant shift in our ideal job outcomes after completing assignment 1 and 2. We all feel that we will remain on course to continue learning more about our ideal roles and what is required to achieve success. All job prospects of our team members involve skills in analysis or problem-solving, strong communication skills, and the ability to work as an integral and high functioning member of a team. As a group there are also some differences among our career aspirations, in particular with the type of industry pathways we wish to pursue and the technologies we may focus on. The differences that also stand out are the specific skills pathway such as cyber security and software development. Also, some of the roles we all wish to pursue would require varied levels of experience to achieve the desired role, which means that we would need to pursue other entry level roles first to gain the required experience before being ready to take on our ideal job. As mentioned in our previous assignment, despite the differences, our ideal jobs are all important components of an IT development software pathway. And can all contribute in different ways to the successful delivery of a software solution.</p>
+          </div>
+          <div>
+            <h2>Sarah S.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Sam K.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Sam B.</h2>
+            <p>As someone who has recently completed their studies in cybersecurity, I have made the decision to further my education and skills in software development by enrolling in a Bachelor of IT program at RMIT. Both fields have always interested me, and I believe that combining my knowledge and skills in both areas will make me a valuable asset in the workforce. After researching data from Burning Glass, I am even more certain that pursuing a career in software development with a focus on cybersecurity is the right path for me. In today's workforce, it is crucial to have a diverse range of skills and knowledge, and I am confident that my background in cybersecurity and my interest in software development will make me an asset to any organization. To ensure that I am providing the best possible service to my future clients or employers, I am committed to continuing my education and staying up to date on the latest developments and trends in both fields.</p>
+          </div>
+          <div>
+            <h2>Ally H.</h2>
+            <p>Hellooo</p>
+          </div>
+          <div>
+            <h2>Damian W.</h2>
+            <p>Hellooo</p>
           </div>
         </div>
       </div>
