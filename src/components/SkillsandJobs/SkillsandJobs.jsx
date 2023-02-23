6887b70c4beb83243522a391184c3938c6efe3ed
skdxxb95 <s3992963@student.rmit.edu.au>
2023-02-23 14:01:23 +1000

    Wrote Skills and Jobs section

--- src/components/SkillsandJobs/SkillsandJobs.jsx
+++ src/components/SkillsandJobs/SkillsandJobs.jsx
@@ -9,8 +9,49 @@ const SkillsandJobs = () => {
             <h1>Skills and Jobs</h1>
             <h5>Who we want to take with us to the next phase.</h5>
             <div>
-              <h2>Test</h2>
-              <p>Hellooo</p>
+            <h2>Position Description 1: Hardware Engineer</h2>
+            <p>The Hardware Engineer will be responsible for designing and developing the DuckyPass device's hardware. This includes the selection of components, PCB design, testing, and optimization. The ideal candidate will have a strong background in hardware design and development, experience with microcontrollers, and a passion for building innovative and reliable products. </p>
+            <p>Responsibilities include:</p>
+            <ul>
+            <li>Design and develop the DuckyPass device's hardware, including component selection, PCB design, and testing.</li>
+            <li>Collaborate with software engineers to ensure seamless integration between hardware and software components.</li>
+            <li>Conduct testing and debugging of hardware components and systems to ensure optimal performance.</li>
+            <li>Work closely with the team to ensure project milestones are met on time and within budget.</li>
+            <li>Conduct research and development to identify new technologies and design approaches that can enhance the DuckyPass device's hardware performance and features.</li>
+            </ul>
+            <br></br>
+            <h2>Position Description 2: Software Engineer</h2>
+            <p>The Software Engineer will be responsible for designing and developing the DuckyPass device's software, including the firmware and the mobile app. The ideal candidate will have experience with embedded software development, mobile app development, and a strong understanding of encryption and security protocols. </p>
+            <p>Responsibilities include:</p>
+            <ul>
+            <li>Develop and optimize the firmware for the DuckyPass device to ensure optimal performance, security, and functionality.</li>
+            <li>Develop and maintain the mobile app that connects to the DuckyPass device using Bluetooth.</li>
+            <li>Conduct testing and debugging of the software components to ensure optimal performance and functionality.</li>
+            <li>Work closely with the hardware team to ensure seamless integration between hardware and software components.</li>
+            <li>Conduct research and development to identify new technologies and design approaches that can enhance the DuckyPass device's software performance and features.</li>
+            </ul>
+            <br></br>
+            <h2>Position Description 3: Product Manager</h2>
+            <p>The Product Manager will be responsible for managing the development and launch of the DuckyPass device. This includes coordinating with the hardware and software teams, conducting market research, defining product requirements, and creating a product roadmap. The ideal candidate will have experience in product management, project management, and a strong understanding of customer needs and market trends. </p>
+            <p>Responsibilities include:</p>
+            <ul>
+            <li>Define product requirements and features based on market research and customer feedback.</li>
+            <li>Develop and maintain the product roadmap and ensure alignment with the company's overall strategy.</li>
+            <li>Work closely with the hardware and software teams to ensure the development of high-quality, reliable, and innovative products.</li>
+            <li>Conduct market research to identify new opportunities and trends in the password management market.</li>
+            <li>Monitor and analyze key performance metrics to ensure the product's success and make data-driven decisions.</li>
+            </ul>
+            <br></br>
+            <h2>Position Description 4: Quality Assurance Engineer</h2>
+            <p>The Quality Assurance Engineer will be responsible for ensuring the DuckyPass device meets the highest standards of quality and security. The ideal candidate will have experience in quality assurance, testing, and a strong understanding of security protocols. </p>
+            <p>Responsibilities include:</p>
+            <ul>
+            <li>Develop and execute test plans to ensure the device meets the highest standards of quality and security.</li>
+            <li>Conduct security testing to identify vulnerabilities and recommend solutions.</li>
+            <li>Collaborate with the hardware and software teams to ensure seamless integration between hardware and software components.</li>
+            <li>Conduct post-release testing and analysis to ensure the device is performing as expected and identify areas for improvement.</li>
+            <li>Develop and maintain documentation related to testing and quality assurance.</li>
+            </ul>
           </div>
         </div>
       </div>
